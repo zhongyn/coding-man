b12e76b5bd3e7f9a322cbf4e317e2d47614a49f9
Yaonan Zhong <zhongyaonan@gmail.com>
2015-06-07 10:59:24 -0700

    rectarea

---
+++ RectArea/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int share = Math.max(0, (Math.min(C, G) - Math.max(A, E)) * (Math.min(D, H) - Math.max(B, F)));
+        return ((C - A) * (D - B) + (G - E) * (H - F)) - share;
+    }
+}
